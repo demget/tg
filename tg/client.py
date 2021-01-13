5bc004f1f2120640054dc8d4d0e3a837dbb9aed8
Demian <d@aletheia.icu>
2021-01-14 01:28:45 +0200

    executor: use subprocess for calling exec commands

--- tg/client.py
+++ tg/client.py
@@ -1,5 +1,5 @@
 from pyrogram import Client, filters
-from tg import HEADER
+from tg import HEADER, logger
 from tg.loader import packages, commands
 from tg.executor import *
 
@@ -25,14 +25,22 @@ async def on_command(_, msg):
     args = msg.text[1:].split(" ")
     name, args = args[0], args[1:]
 
-    res = execute_command(name, args)
-    if callable(res):
-        await res(client, msg)
-    else:
-        await msg.edit(res)
+    try:
+        res = execute_command(name, args)
+        if callable(res):
+            await res(client, msg)
+        else:
+            await msg.edit(res)
+    except Exception as e:
+        logger.error(e)
+        await msg.edit(e)
 
 
 @client.on_message(filters.me & filters.regex(r"^\!"))
 async def on_exec(_, msg):
-    out = execute_shell(msg.text[1:])
-    await msg.edit(f"<pre>{out}</pre>")
+    try:
+        out = execute_shell(msg.text[1:])
+        await msg.edit(f"<pre>{out}</pre>")
+    except Exception as e:
+        logger.error(e)
+        await msg.edit(e)
--- tg/executor.py
+++ tg/executor.py
@@ -1,15 +1,27 @@
-import os
 import re
+import subprocess
 from tg.loader import packages, commands
+
+
+ERROR_MESSAGE = "Command not found"
+re_shell = re.compile(r"\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])")
 
 
 def execute_command(name, args):
     if name not in commands:
-        return "Command not found"
+        return ERROR_MESSAGE
     return commands[name](*args)
     
 
 def execute_shell(exec):
-    out = os.popen(exec).read()    
-    out = re.sub(r"\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])", "", out)
-    return out
+    bash = subprocess.Popen(
+        ["/bin/bash"],
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        shell=True
+    )
+
+    out, _ = bash.communicate(exec.encode())
+    if not out:
+        return ERROR_MESSAGE
+    return re_shell.sub("", out.decode())
